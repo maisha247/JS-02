ca4c598a14cf3c3e6c9593e85c9f7e47f6c3f0e5
Maisha Tasnim Mridula <maishatasnimmridula@gmail.com>
2025-10-26 21:51:23 +0600

    js-02

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// example#01
+//    let age = 20
+//    if (age > 19) {
+//     alert("go")
+//    } else {
+//     alert("Don't go")
+//    }
+// ============================================
+// example#02
+//    let age = 20
+//    if (age < 19) {
+//     alert("go")
+//    } else {
+//     alert("Don't go")
+//    }
+// ============================================
+// example#03
+//    let location = Dhaka
+//    console.log(location);
+
+//    result: syntax error;
+//    N.B. there is a reserved keyword named "location". That's why it's not couted as a variable.
+// ============================================
+// example#04
+//    let area = "Dhaka"
+//    console.log(area);
+
+//    result: Dhaka
+//    N.B. we have to use double quotation to use the variable.
+// ============================================
+// example#05
+//    let area = "Dhaka"
+//    if (area == "Dhaka") {
+//     console.log("Location is Right");
+//    }
+// ============================================
+// example#06
+//    let area = "CTG"
+//    if (area == "Dhaka") {
+//     console.log("Location is Correct");
+//    } else {
+//     console.log("Location is Wrong");
+//    }
+// ============================================
+
+   
+   
