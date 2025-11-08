15ec3ff77f2f1c1cdc2553fa3bf2c5d9172a902e
Maisha Tasnim Mridula <maishatasnimmridula@gmail.com>
2025-11-08 12:19:56 +0600

    js-02

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@
 //    } else {
 //     alert("Don't go")
 //    }
+
 // ============================================
+
 // example#02
 //    let age = 20
 //    if (age < 19) {
@@ -13,35 +15,70 @@
 //    } else {
 //     alert("Don't go")
 //    }
+
 // ============================================
+
 // example#03
 //    let location = Dhaka
 //    console.log(location);
 
 //    result: syntax error;
 //    N.B. there is a reserved keyword named "location". That's why it's not couted as a variable.
+
 // ============================================
+
 // example#04
 //    let area = "Dhaka"
 //    console.log(area);
 
 //    result: Dhaka
 //    N.B. we have to use double quotation to use the variable.
+
 // ============================================
+
 // example#05
 //    let area = "Dhaka"
 //    if (area == "Dhaka") {
 //     console.log("Location is Right");
 //    }
+
 // ============================================
+
 // example#06
 //    let area = "CTG"
 //    if (area == "Dhaka") {
 //     console.log("Location is Correct");
-//    } else {
+//    } 
+//    else {
 //     console.log("Location is Wrong");
 //    }
+
 // ============================================
 
-   
-   
+// example#07
+// let name = "Maisha"
+// let friendName = "Israt"
+// console.log(`My name is ${name}. My friends name is ${friendName}.`);
+
+// result: My name is Maisha. My friends name is Israt.
+
+// ============================================
+
+// ===============FOR LOOP===================
+// example#01
+// for(i=1; i<11; i++){
+//    console.log(i);
+// }
+
+// result: 1
+//         2
+//         3
+//         4
+//         5
+//         6
+//         7
+//         8
+//         9
+//         10
+// ===============FOR LOOP===================
+
